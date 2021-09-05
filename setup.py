7289352ee5f0a829c21a71f131ad34df9f3c3c24
Sam Stenvall <sam.stenvall@nitor.com>
2021-09-05 12:39:13 +0300

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pycaruna",
-    version="0.0.1",
+    version="0.0.2",
     author="Sam Stenvall",
     author_email="neggelandia@gmail.com",
     description="Caruna API Python implementation",
